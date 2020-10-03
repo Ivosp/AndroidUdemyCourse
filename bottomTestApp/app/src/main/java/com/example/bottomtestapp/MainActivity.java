63ca25927fe5223e52f3935fe0af609360126b62
Ivo Specht <ivo.specht@mail.com>
2020-10-03 18:21:28 +0100

    Added some functionality

--- bottomTestApp/app/src/main/java/com/example/bottomtestapp/MainActivity.java
+++ bottomTestApp/app/src/main/java/com/example/bottomtestapp/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.bottomtestapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.app_bar_menu, menu);
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menuSettings:
+                Toast.makeText(this, "settings were pressed", Toast.LENGTH_SHORT).show();
+
+                return true;
+            case R.id.menuAbout:
+                Toast.makeText(this, "about was pressed", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.signup:
+                Toast.makeText(this, "signup was pressed", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.account:
+                Toast.makeText(this, "Account was pressed", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
